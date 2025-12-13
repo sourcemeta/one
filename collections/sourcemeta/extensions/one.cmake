5204316441fe591231c44914fb5fdab6ad550d31
Juan Cruz Viotti <jv@jviotti.com>
2025-12-13 14:29:01 +0100

    Add a `@sourcemeta/extensions` built-in collection (#582)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ collections/sourcemeta/extensions/one.cmake
@@ -0,0 +1,16 @@
+set(COLLECTION_NAMESPACE "sourcemeta/extensions")
+set(EXTENSIONS_PATH "${PROJECT_SOURCE_DIR}/vendor/collections/${COLLECTION_NAMESPACE}")
+
+install(DIRECTORY "${EXTENSIONS_PATH}/schemas"
+  DESTINATION "${CMAKE_INSTALL_DATADIR}/sourcemeta/one/collections/${COLLECTION_NAMESPACE}"
+  COMPONENT sourcemeta_one)
+install(FILES "${CMAKE_CURRENT_LIST_DIR}/one.json"
+  DESTINATION "${CMAKE_INSTALL_DATADIR}/sourcemeta/one/collections/${COLLECTION_NAMESPACE}"
+  COMPONENT sourcemeta_one)
+
+configure_file("${CMAKE_CURRENT_LIST_DIR}/jsonschema.json"
+  "${CMAKE_CURRENT_BINARY_DIR}/${COLLECTION_NAMESPACE}.json" @ONLY)
+install(FILES "${CMAKE_CURRENT_BINARY_DIR}/${COLLECTION_NAMESPACE}.json"
+  DESTINATION "${CMAKE_INSTALL_DATADIR}/sourcemeta/one/collections/${COLLECTION_NAMESPACE}"
+  RENAME jsonschema.json
+  COMPONENT sourcemeta_one)
