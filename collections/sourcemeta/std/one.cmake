f0f4afd58093341a80c4abcb04c3e645b3460367
Juan Cruz Viotti <jv@jviotti.com>
2025-12-29 14:01:41 -0400

    [WIP] Flatten the standard library
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- collections/sourcemeta/std/one.cmake
+++ collections/sourcemeta/std/one.cmake
@@ -1,16 +1,13 @@
-set(COLLECTION_NAMESPACE "sourcemeta/std/v0")
-set(STD_PATH "${PROJECT_SOURCE_DIR}/vendor/collections/${COLLECTION_NAMESPACE}")
+set(COLLECTION_NAMESPACE "sourcemeta/std")
+set(STD_PATH "${PROJECT_SOURCE_DIR}/vendor/collections/${COLLECTION_NAMESPACE}/v0")
 
 install(DIRECTORY "${STD_PATH}/schemas"
   DESTINATION "${CMAKE_INSTALL_DATADIR}/sourcemeta/one/collections/${COLLECTION_NAMESPACE}"
   COMPONENT sourcemeta_one)
-install(FILES "${CMAKE_CURRENT_LIST_DIR}/one.json"
-  DESTINATION "${CMAKE_INSTALL_DATADIR}/sourcemeta/one/collections/${COLLECTION_NAMESPACE}"
-  COMPONENT sourcemeta_one)
 
-configure_file("${CMAKE_CURRENT_LIST_DIR}/jsonschema.json"
+configure_file("${CMAKE_CURRENT_LIST_DIR}/one.json"
   "${CMAKE_CURRENT_BINARY_DIR}/${COLLECTION_NAMESPACE}.json" @ONLY)
 install(FILES "${CMAKE_CURRENT_BINARY_DIR}/${COLLECTION_NAMESPACE}.json"
   DESTINATION "${CMAKE_INSTALL_DATADIR}/sourcemeta/one/collections/${COLLECTION_NAMESPACE}"
-  RENAME jsonschema.json
+  RENAME one.json
   COMPONENT sourcemeta_one)
