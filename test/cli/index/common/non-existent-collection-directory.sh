920e5dd96d1ea0628a562da332a3907be6dad5b0
Juan Cruz Viotti <jv@jviotti.com>
2026-02-24 12:29:55 -0400

    Gracefully skip non-existent collection directories (#662)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ test/cli/index/common/non-existent-collection-directory.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+TMP="$(mktemp -d)"
+clean() { rm -rf "$TMP"; }
+trap clean EXIT
+
+cat << EOF > "$TMP/one.json"
+{
+  "url": "http://localhost:8000",
+  "html": false,
+  "contents": {
+    "example": {
+      "baseUri": "https://example.com",
+      "path": "./schemas"
+    }
+  }
+}
+EOF
+
+"$1" "$TMP/one.json" "$TMP/output"
+
+cd "$TMP/output"
+find . -mindepth 1 | LC_ALL=C sort > "$TMP/manifest.txt"
+cd - > /dev/null
+
+cat << 'EOF' > "$TMP/expected.txt"
+./configuration.json
+./dependency-tree.metapack
+./dependency-tree.metapack.deps
+./explorer
+./explorer/%
+./explorer/%/directory.metapack
+./explorer/%/directory.metapack.deps
+./explorer/%/search.metapack
+./explorer/%/search.metapack.deps
+./routes.bin
+./routes.bin.deps
+./version.json
+EOF
+
+diff "$TMP/manifest.txt" "$TMP/expected.txt"
