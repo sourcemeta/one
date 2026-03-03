095ab251113ff0c079960a9e783a364743d2ff07
github-action-benchmark <github@users.noreply.github.com>
2026-03-03 13:59:25 +0000

    add Benchmark Index (enterprise) (customSmallerIsBetter) benchmark result for 302db472f9e387ed57f82955b58c2c2a8de238a6

---
+++ benchmark/enterprise/index/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1772546365610,
+  "repoUrl": "https://github.com/sourcemeta/one",
+  "entries": {
+    "Benchmark Index (enterprise)": [
+      {
+        "commit": {
+          "author": {
+            "email": "jv@jviotti.com",
+            "name": "Juan Cruz Viotti",
+            "username": "jviotti"
+          },
+          "committer": {
+            "email": "jv@jviotti.com",
+            "name": "Juan Cruz Viotti",
+            "username": "jviotti"
+          },
+          "distinct": true,
+          "id": "302db472f9e387ed57f82955b58c2c2a8de238a6",
+          "message": "Run CI pipeline on `main` too (for benchmarks)\n\nSigned-off-by: Juan Cruz Viotti <jv@jviotti.com>",
+          "timestamp": "2026-03-03T09:48:13-04:00",
+          "tree_id": "f855fc24841c07dca7d99ed0980bcd7daeedd997",
+          "url": "https://github.com/sourcemeta/one/commit/302db472f9e387ed57f82955b58c2c2a8de238a6"
+        },
+        "date": 1772546364343,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Add One Schema",
+            "value": 48,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
