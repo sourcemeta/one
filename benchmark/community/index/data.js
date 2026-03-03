177691c8fbe59218611a812d9caef105e47f3bbd
github-action-benchmark <github@users.noreply.github.com>
2026-03-03 15:53:03 +0000

    add Benchmark Index (community) (customSmallerIsBetter) benchmark result for c6157d325dc3225163af08edd90e0fb061587df0

--- benchmark/community/index/data.js
+++ benchmark/community/index/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772546340640,
+  "lastUpdate": 1772553183677,
   "repoUrl": "https://github.com/sourcemeta/one",
   "entries": {
     "Benchmark Index (community)": [
@@ -28,6 +28,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "Add One Schema",
             "value": 49,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jv@jviotti.com",
+            "name": "Juan Cruz Viotti",
+            "username": "jviotti"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c6157d325dc3225163af08edd90e0fb061587df0",
+          "message": "Increase indexing benchmarks based on registry size (#693)\n\nSigned-off-by: Juan Cruz Viotti <jv@jviotti.com>",
+          "timestamp": "2026-03-03T11:41:40-04:00",
+          "tree_id": "d4d86233da495ad98987fb117b75e6491a4c0152",
+          "url": "https://github.com/sourcemeta/one/commit/c6157d325dc3225163af08edd90e0fb061587df0"
+        },
+        "date": 1772553182382,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Add one schema (0 existing)",
+            "value": 47,
+            "unit": "ms"
+          },
+          {
+            "name": "Add one schema (100 existing)",
+            "value": 759,
+            "unit": "ms"
+          },
+          {
+            "name": "Add one schema (1000 existing)",
+            "value": 7629,
             "unit": "ms"
           }
         ]
