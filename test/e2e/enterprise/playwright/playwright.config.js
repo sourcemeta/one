bb0c4a550d69f6de9ffc3223fd18ebe8b8e17c55
Juan Cruz Viotti <jv@jviotti.com>
2026-03-04 16:21:00 -0400

    Simpler enterprise
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/e2e/enterprise/playwright/playwright.config.js
+++
@@ -1,7 +0,0 @@
-import base from '../../html/playwright/playwright.config.js';
-import { defineConfig } from '@playwright/test';
-
-export default defineConfig({
-  ...base,
-  testDir: '../../html/playwright'
-});
