708419a7e65e172ea13e67e552bf080b82955a0f
Juan Cruz Viotti <jv@jviotti.com>
2026-03-25 14:32:29 -0400

    Automatically ignore configuration files in collection paths
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ test/cli/index/common/collection-path-empty.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+TMP="$(mktemp -d)"
+clean() { rm -rf "$TMP"; }
+trap clean EXIT
+
+cat << EOF > "$TMP/one.json"
+{
+  "url": "https://sourcemeta.com/",
+  "contents": {
+    "example": {
+      "contents": {
+        "schemas": {
+          "baseUri": "https://example.com/",
+          "path": ""
+        }
+      }
+    }
+  }
+}
+EOF
+
+"$1" --skip-banner "$TMP/one.json" "$TMP/output" --concurrency 1 2> /dev/null
