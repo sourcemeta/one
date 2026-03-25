3328c68b7f07ffa760a81f6128fd3b78c9a52b4a
Juan Cruz Viotti <jv@jviotti.com>
2026-03-25 15:14:22 -0400

    More
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/cli/index/common/collection-path-dot.sh
+++ test/cli/index/common/collection-path-dot.sh
@@ -23,4 +23,28 @@ cat << EOF > "$TMP/one.json"
 }
 EOF
 
-"$1" --skip-banner "$TMP/one.json" "$TMP/output" --concurrency 1 2> /dev/null
+"$1" --skip-banner "$TMP/one.json" "$TMP/output" --concurrency 1 \
+  2> "$TMP/output.txt" && CODE="$?" || CODE="$?"
+test "$CODE" = "0" || exit 1
+
+# Remove thread information
+if [ "$(uname)" = "Darwin" ]
+then
+  sed -i '' 's/ \[.*\]//g' "$TMP/output.txt"
+else
+  sed -i 's/ \[.*\]//g' "$TMP/output.txt"
+fi
+
+cat << EOF > "$TMP/expected.txt"
+Writing output to: $(realpath "$TMP")/output
+Using configuration: $(realpath "$TMP")/one.json
+( 14%) Producing: configuration.json
+( 28%) Producing: version.json
+( 42%) Producing: explorer/%/404.metapack
+( 57%) Producing: explorer/%/directory.metapack
+( 71%) Producing: explorer/%/directory-html.metapack
+( 85%) Producing: explorer/%/search.metapack
+(100%) Producing: routes.bin
+EOF
+
+diff "$TMP/output.txt" "$TMP/expected.txt"
