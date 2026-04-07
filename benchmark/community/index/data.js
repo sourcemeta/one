11ec53b3c6bcda2d0a556eae6ef1167cce7f43a0
github-action-benchmark <github@users.noreply.github.com>
2026-04-07 18:53:35 +0000

    add Benchmark Index (community) (customSmallerIsBetter) benchmark result for 102d7df4ebcc9f9ce35047a18aaf54acb1c16083

--- benchmark/community/index/data.js
+++ benchmark/community/index/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1775583501430,
+  "lastUpdate": 1775588014997,
   "repoUrl": "https://github.com/sourcemeta/one",
   "entries": {
     "Benchmark Index (community)": [
@@ -9551,6 +9551,105 @@ window.BENCHMARK_DATA = {
           {
             "name": "Index 10000 schemas",
             "value": 13101,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jv@jviotti.com",
+            "name": "Juan Cruz Viotti",
+            "username": "jviotti"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "102d7df4ebcc9f9ce35047a18aaf54acb1c16083",
+          "message": "Add a new guide entry on incrementally adopting schema governance (#815)\n\nSigned-off-by: Juan Cruz Viotti <jv@jviotti.com>",
+          "timestamp": "2026-04-07T14:29:40-04:00",
+          "tree_id": "8b2be9dc3fbc04ca31936a79482fb1bccc82b8f2",
+          "url": "https://github.com/sourcemeta/one/commit/102d7df4ebcc9f9ce35047a18aaf54acb1c16083"
+        },
+        "date": 1775588014325,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Add one schema (0 existing)",
+            "value": 18,
+            "unit": "ms"
+          },
+          {
+            "name": "Add one schema (100 existing)",
+            "value": 23,
+            "unit": "ms"
+          },
+          {
+            "name": "Add one schema (1000 existing)",
+            "value": 78,
+            "unit": "ms"
+          },
+          {
+            "name": "Add one schema (10000 existing)",
+            "value": 892,
+            "unit": "ms"
+          },
+          {
+            "name": "Update one schema (1 existing)",
+            "value": 16,
+            "unit": "ms"
+          },
+          {
+            "name": "Update one schema (101 existing)",
+            "value": 23,
+            "unit": "ms"
+          },
+          {
+            "name": "Update one schema (1001 existing)",
+            "value": 79,
+            "unit": "ms"
+          },
+          {
+            "name": "Update one schema (10001 existing)",
+            "value": 815,
+            "unit": "ms"
+          },
+          {
+            "name": "Cached rebuild (1 existing)",
+            "value": 9,
+            "unit": "ms"
+          },
+          {
+            "name": "Cached rebuild (101 existing)",
+            "value": 11,
+            "unit": "ms"
+          },
+          {
+            "name": "Cached rebuild (1001 existing)",
+            "value": 33,
+            "unit": "ms"
+          },
+          {
+            "name": "Cached rebuild (10001 existing)",
+            "value": 320,
+            "unit": "ms"
+          },
+          {
+            "name": "Index 100 schemas",
+            "value": 121,
+            "unit": "ms"
+          },
+          {
+            "name": "Index 1000 schemas",
+            "value": 998,
+            "unit": "ms"
+          },
+          {
+            "name": "Index 10000 schemas",
+            "value": 12810,
             "unit": "ms"
           }
         ]
