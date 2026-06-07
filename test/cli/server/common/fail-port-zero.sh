578c6c34d4c291e308986f969e73f878da595c4d
Juan Cruz Viotti <jv@jviotti.com>
2026-06-07 18:33:47 -0400

    Improve argument and etag parsing (#1027)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ test/cli/server/common/fail-port-zero.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+TMP="$(mktemp -d)"
+clean() { rm -rf "$TMP"; }
+trap clean EXIT
+
+"$1" /tmp "0" > "$TMP/output.txt" 2>&1 && CODE="$?" || CODE="$?"
+test "$CODE" = "1" || exit 1
+
+cat << EOF > "$TMP/expected.txt"
+Sourcemeta One $2 v$3
+Usage: $(basename "$1") <path/to/output/directory> <port>
+error: The port must be a valid TCP port
+EOF
+
+diff "$TMP/output.txt" "$TMP/expected.txt"
