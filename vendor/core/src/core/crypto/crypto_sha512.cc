07621370d200470d36320686577851171e1d033e
Juan Cruz Viotti <jv@jviotti.com>
2026-06-15 14:10:29 -0400

    Upgrade Core to `708730fe29acd954ab5fb26b7e5e5f9de07927d4` (#1058)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- vendor/core/src/core/crypto/crypto_sha512.cc
+++ vendor/core/src/core/crypto/crypto_sha512.cc
@@ -1,4 +1,4 @@
-#include <sourcemeta/core/crypto_sha256.h>
+#include <sourcemeta/core/crypto_sha512.h>
 #include <sourcemeta/core/text.h>
 
 #include <ostream>     // std::ostream, std::streamsize
@@ -7,14 +7,14 @@
 
 namespace sourcemeta::core {
 
-auto sha256(const std::string_view input) -> std::string {
-  const auto digest{sha256_digest(input)};
+auto sha512(const std::string_view input) -> std::string {
+  const auto digest{sha512_digest(input)};
   return bytes_to_hex(
       {reinterpret_cast<const char *>(digest.data()), digest.size()});
 }
 
-auto sha256(const std::string_view input, std::ostream &output) -> void {
-  const auto result = sha256(input);
+auto sha512(const std::string_view input, std::ostream &output) -> void {
+  const auto result = sha512(input);
   output.write(result.data(), static_cast<std::streamsize>(result.size()));
 }
 
