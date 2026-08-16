61e2f16ad9725a0e95d25b95ef9aa8af1b841b49
Juan Cruz Viotti <jv@jviotti.com>
2026-08-16 20:19:25 -0300

    Disable `-march=native` on the JSON Schema CLI for portability reasons (#1227)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- cmake/FindJSONSchema.cmake
+++ cmake/FindJSONSchema.cmake
@@ -3,6 +3,9 @@ if(NOT JSONSchema_FOUND)
     set(JSONSCHEMA_USE_SYSTEM_CURL ON CACHE BOOL "Enable system cURL")
   endif()
 
+  # For maximum processor compatibility
+  set(JSONSCHEMA_PORTABLE ON CACHE BOOL "Build a portable JSON Schema CLI binary")
+
   add_subdirectory("${PROJECT_SOURCE_DIR}/vendor/jsonschema")
   set(JSONSchema_FOUND ON)
 endif()
